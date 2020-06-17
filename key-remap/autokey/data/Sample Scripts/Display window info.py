229647649df1f70730619fa379a296aa890e351d
Rafael Vega <contacto@rafaelvega.co>
2020-06-17 09:49:09 -0500

    Dont send modified keys to VirtualBox

--- key-remap/autokey/data/Sample Scripts/Display window info.py
+++ key-remap/autokey/data/Sample Scripts/Display window info.py
@@ -1,7 +1,7 @@
 # Displays the information of the next window to be left-clicked
 import time
-mouse.wait_for_click(1)
-time.sleep(0.2)
+#mouse.wait_for_click(1)
+#time.sleep(0.2)
 winTitle = window.get_active_title()
 winClass = window.get_active_class()
 dialog.info_dialog("Window information", 
